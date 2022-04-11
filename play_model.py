5ae5afeebdb48b5b6c13444b3f9c8aa8dc81ba02
soheil-h <habibian@vt.edu>
2022-04-11 14:48:54 -0400

    new-dems

--- play_model.py
+++ play_model.py
@@ -83,7 +83,7 @@ class JoystickControl(object):
 class Model(object):
     def __init__(self):
         self.model = BC(32)
-        model_dict = torch.load("models/" + args.set + "/MLP_model_1", map_location='cpu')
+        model_dict = torch.load("models/" + args.set + "/MLP_model_3", map_location='cpu')
         self.model.load_state_dict(model_dict)
         self.model.eval
 
